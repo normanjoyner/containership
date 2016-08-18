b6878f608426484bd294660c86c4a94802d7c9e6
Norman Joyner <norman.joyner@gmail.com>
2016-08-18 12:25:46 -0400

    removes daemonizing (#162)

--- commands/agent.js
+++ commands/agent.js
@@ -3,7 +3,6 @@
 const pkg = require('../package.json');
 
 const _ = require('lodash');
-const daemon = require('daemon');
 const fs = require('fs');
 
 const PID_FILE = '/var/run/containership.pid';
@@ -18,13 +17,6 @@ module.exports = {
                     options: core.options,
 
                     callback: (options) => {
-                        if(process.env.NODE_ENV !== 'development') {
-                            daemon({
-                                stdout: process.stdout,
-                                stderr: process.stderr
-                            });
-                        }
-
                         fs.writeFile(PID_FILE, process.pid, (err) => {
                             if(err) {
                                 process.stderr.write('Error writing PID! Are you running containership as root?\n');
